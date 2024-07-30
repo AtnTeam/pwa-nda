c8e2b5b21a44ce64da34fb9d78f0e6fc203680c3
koyteam <133971790+AtnTeam@users.noreply.github.com>
2024-07-30 11:50:46 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,59 +1,3 @@
-// const publicVapidKey = 'BCRndj3xy22bI2uTzqXcUXDYretU47YZ84gPxctPQh258wgo6E-LNs54mXpYWLwxrTG53MiPk6ad3hdvqy48Qoc';
-
-// // Check for service worker
-
-// if ('serviceWorker' in navigator) {
-//     send().catch(err => console.error(err));
-// }
-
-// // Register SW, Register Push, Send Push
-
-// async function send() {
-//     // Register Service Worker
-//     console.log('Register service workeer ...');
-//     const register = await navigator.serviceWorker.register('/worker.js', {
-//         scope: '/' // звернення до сервера
-//     });
-
-//     console.log('Service Worker Registered ...');
-
-//     // Register Push
-//     console.log("Registering Push...");
-//     const subscription = await register.pushManager.subscribe({
-//         userVisibleOnly: true,
-//         applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-//     });
-//     console.log("Push Registered...");
-
-//     // Send Push Notification
-//     console.log("Sending Push...");
-//     await fetch("/subscribe", {
-//         method: "POST",
-//         body: JSON.stringify(subscription),
-//         headers: {
-//             "content-type": "application/json"
-//         }
-//     });
-//     console.log("Push Sent...");
-// }
-
-// function urlBase64ToUint8Array(base64String) {
-//     const padding = "=".repeat((4 - base64String.length % 4) % 4);
-//     const base64 = (base64String + padding)
-//         .replace(/\-/g, "+")
-//         .replace(/_/g, "/");
-
-//     const rawData = window.atob(base64);
-//     const outputArray = new Uint8Array(rawData.length);
-
-//     for (let i = 0; i < rawData.length; ++i) {
-//         outputArray[i] = rawData.charCodeAt(i);
-//     }
-//     return outputArray;
-// }
-
-// ========
-
 const publicVapidKey = 'BCRndj3xy22bI2uTzqXcUXDYretU47YZ84gPxctPQh258wgo6E-LNs54mXpYWLwxrTG53MiPk6ad3hdvqy48Qoc';
 
 // Check for service worker
@@ -65,8 +9,8 @@ if ('serviceWorker' in navigator) {
 async function registerServiceWorkerAndSubscribe() {
     // Register Service Worker
     console.log('Registering service worker...');
-    const registration = await navigator.serviceWorker.register('/worker.js', {
-        scope: '/'
+    const registration = await navigator.serviceWorker.register('./worker.js', {
+        scope: './'
     });
 
     console.log('Service Worker Registered...');
